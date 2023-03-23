e68c46b27afaa7827fbcb39c56a1d33e3b66b619
yaswanthachanta <94340507+yaswanthachanta@users.noreply.github.com>
2023-03-23 10:19:33 +0530

    Update CSE 310 WEEK1-DAY1-Q1.java

--- CSE 310 WEEK1-DAY1-Q1.java
+++ CSE 310 WEEK1-DAY1-Q1.java
@@ -1,1 +1,18 @@
+/* question 1 Write a program to take the 2 inputs of integer type from the user. Display the values separated by symbol ‘-’ if the values are positive otherwise display
+the message “Invalid Input”.*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a > 0 && b > 0) {
+            System.out.println(a + "-" + b);
+        } else {
+            System.out.println("Invalid Input");
+}
+}
+}
 
